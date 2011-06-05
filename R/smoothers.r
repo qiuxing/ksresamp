d56aa12794f39d4fbaca4fa542a922498e096bb0
Xing Qiu <xing.qiu@gmail.com>
2011-06-05 11:28:59 -0400

    sm.method() can be a custom function now.

--- R/smoothers.r
+++ R/smoothers.r
@@ -24,16 +24,20 @@
 .tps.md <- function(grids, yarray, bandwidth=5.0, ...){
   x <- expand.grid(grids); Y <- as.vector(yarray)
   ## mod1 <- fastTps(x, Y, theta=theta, ...)
-  mod1 <- Tps(x, Y, theta=bandwidth, ...)
+  mod1 <- fastTps(x, Y, theta=bandwidth, ...)
   return(array(predict(mod1), dim(yarray)))
 }
 
 ## wrapper function for different spatial smoothers
 Smooth <- function(grids, yarray, bandwidth=5.0, sm.method="ksmooth", ...) {
-  ys <- switch(sm.method,
-               "ksmooth"=.ksmooth.md(grids, yarray, bandwidth, ...),
-               "tps"=.tps.md(grids, yarray, bandwidth, ...),
-               stop("Valid smoothing methods: 1. ksmooth (kernel smoothing, the default choice), 2. tps (thin plate spline)."))
-  return(ys)
-}
+  if (!is.function(sm.method)){
+    ys <- switch(sm.method,
+                 "ksmooth"=.ksmooth.md(grids, yarray, bandwidth, ...),
+                 "tps"=.tps.md(grids, yarray, bandwidth, ...),
+                 stop("Valid smoothing methods: 1. ksmooth (kernel smoothing, the default choice), 2. tps (thin plate spline)."))
+  } else {                 #sm.method() is a custom smoothing function
+    ys <- sm.method(grids, yarray, bandwidth, ...)
+  }
+    return(ys)
+  }
 
